9bc4af9d76336ca4abd425a807326a7c3fd7c42a
LeonardoRubuz <rubuzleonardo@gmail.com>
2024-03-19 09:37:21 +0100

    feat (app) : add showAppOptions state This state is set to true whenever the user pick a picture and when true, displays three new buttons

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ const PlaceHolderImage = require('./assets/images/background-image.png')
 
 export default function App() {
   const [selectedImage, setSelectedImage] = useState(null);
+  const [showAppOptions, setShowAppOptions] = useState(false);
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -18,6 +19,7 @@ export default function App() {
     });
     if (!result.canceled) {
       setSelectedImage(result.assets[0].uri)
+      setShowAppOptions(true);
       console.log(result);
     }else{
       alert('You did not select any image.')
@@ -29,10 +31,16 @@ export default function App() {
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={PlaceHolderImage} selectedImage={selectedImage} />
       </View>
-      <View style>
-        <Button label="Choose a photo" theme="primary" onPress={pickImageAsync}/>
-        <Button label="Use this photo" />
-      </View>
+      {
+        showAppOptions ? (
+          <View /> ) :
+          (
+          <View style={styles.footerContainer}>
+            <Button label="Choose a photo" theme="primary" onPress={pickImageAsync}/>
+            <Button label="Use this photo" onPress={() => setShowAppOptions(true)}/>
+          </View>
+          )
+      }
       <StatusBar style="auto" />
     </View>
   );
--- components/Button.js
+++ components/Button.js
@@ -20,9 +20,7 @@ function Button({label, theme, onPress}) {
     
     return ( 
         <View style={styles.buttonContainer}>
-            <Pressable style={styles.button} onPress={
-                () => alert('You pressed a button')
-            }>
+            <Pressable style={styles.button} onPress={onPress}>
                 <Text style={styles.buttonLabel}> {label} </Text>
             </Pressable>
         </View>
